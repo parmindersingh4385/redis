2e54daac5dc3fb0ce12214e51e12e3b6a2f4563d
parmindersingh4385 <singh1.parminder@gmail.com>
2021-04-02 21:11:57 +0530

    latest

---
+++ index.js
@@ -0,0 +1,13 @@
+var express = require('express'),
+	app = express();
+
+var port = process.env.PORT || 9092;  
+
+
+app.get('/', function(req, res){
+	res.send('Redis api is working fine');
+});
+
+app.listen(port, function(req, res){
+	console.log('App is running on port:-' + port);
+});
